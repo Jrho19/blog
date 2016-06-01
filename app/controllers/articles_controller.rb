5aca8038740ee3f07db672ebe8e9769ea9913dea
John Rhoden <jrho19.jr@gmail.com>
2016-06-01 19:01:31 -0400

    updated create action and post routes for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,13 @@
 class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+    # render plain: params[:article].inspect (initial code for create method)
+
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  post 'articles/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
