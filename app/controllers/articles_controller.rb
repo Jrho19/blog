3d65667aceb50d5309164d6383001c23407a7ed0
John Rhoden <jrho19.jr@gmail.com>
2016-06-07 14:29:13 -0400

    updated create action and added index and  show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,26 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
     # render plain: params[:article].inspect (initial code for create method)
 
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title,:text)
+    end
 end
